d7a5873a3bb69f78313c331fd0891e2a00873a33
Gustavo M Pierre <gustavopierre@gmail.com>
2025-07-02 23:17:12 +0100

    schemas included

---
+++ api/schemas/personality_trait_schema.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+from typing import Optional, List
+import json
+
+class PersonalityTraitSchema(BaseModel):
+    """
+    Schema for personality trait prediction input.
+    """
+    name: str = "John"
+    timeSpentAlone: int = 0
+    stageFear: int = 0
+    socialEventAttendance: int = 0
+    goingOutside: int = 0
+    drainedAfterSocializing: int = 0
+    friendsCircleSize: int = 0
+    postFrequency: int = 0
+    
+class PersonalityTraitResponseSchema(BaseModel):
+    """
+    Schema for personality trait prediction output.
+    """
+    name: str
+    personalityTrait: int
+    
